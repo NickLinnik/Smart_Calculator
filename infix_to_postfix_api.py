06aa9a7814775650bb44545520056b9d604ac456
Nick Linnik <nik.linnik.001@gmaul.com>
2021-08-05 22:56:18 +0300

    basic functionality

---
+++ infix_to_postfix_api.py
@@ -0,0 +1,64 @@
+import re
+from collections import deque
+
+OPERATORS = {'+', '-', '*', '/', '(', ')', '^'}
+
+
+def parse(expression: str):
+    clean_expression = get_expression_with_single_operators(expression)
+    infix_queue = get_infix_queue(clean_expression)
+    stack = deque()
+    queue_postfix = deque()
+    PRIORITY = {'+': 0, '-': 0, '*': 1, '/': 1, '^': 2}
+    while infix_queue:
+        elem = infix_queue.popleft()
+        if elem not in OPERATORS:
+            queue_postfix.append(elem)
+        elif elem == '(':
+            stack.append(elem)
+        elif elem == ')':
+            while stack and stack[-1] != '(':
+                queue_postfix.append(stack.pop())
+            stack.pop()
+        else:
+            while stack and stack[-1] != '(' and PRIORITY[elem] <= PRIORITY[stack[-1]]:
+                queue_postfix.append(stack.pop())
+            stack.append(elem)
+
+    while stack:
+        elem = stack.pop()
+        if elem == '(':
+            raise ArithmeticError
+        queue_postfix.append(elem)
+    return queue_postfix
+
+
+def get_infix_queue(expression: str):
+    expression_list = re.split('([+\\-*/^()])', expression)  # split by operators
+    queue = deque([x for x in expression_list if x != ''])  # get rid of the empty elements
+    if queue[0] == '-':  # '-' might be the first element, it should be a part of the first operand then
+        queue[1] = '-' + queue[1]
+        queue.remove('-')
+    return queue
+
+
+# get rid of all whitespaces and additional operators to get strict sequence of operands and operators
+def get_expression_with_single_operators(old_expression: str):
+    if re.search('\\d+(.\\d+)? +\\d+(.\\d+)?', old_expression):
+        raise ValueError
+    # regex for checking operands without operators between them (only spaces)
+    old_expression = old_expression.replace(' ', '')  # further calculations need expression without spaces
+    while True:
+        new_expression: str = old_expression. \
+            replace('++', '+'). \
+            replace('--', '+'). \
+            replace('+-', '-'). \
+            replace('-+', '-')
+        if old_expression == new_expression:
+            break
+        old_expression = new_expression
+    return new_expression
+
+
+if __name__ == '__main__':
+    print(' '.join(parse(input())))
---
+++ main.py
@@ -0,0 +1,130 @@
+import re
+import sys
+from collections import deque
+
+import infix_to_postfix_api
+
+var_dict = {}
+operations = {
+    '+': lambda a, b: a + b,
+    '-': lambda a, b: a - b,
+    '*': lambda a, b: a * b,
+    '/': lambda a, b: a / b,
+    '^': lambda a, b: a ** b
+}
+
+
+def main():
+    expression = input().strip()
+    while True:
+        if is_command(expression):
+            command = commands.get(expression)
+            command() if command is not None else print('Unknown command')
+        elif '=' in expression:
+            try:
+                pair = expression.replace(' ', '').split('=')
+                if len(pair) != 2:
+                    raise ValueError
+                upd_dict_with_var(pair[0], pair[1])
+            except KeyError:
+                print('Unknown variable')
+            except NameError:
+                print('Invalid identifier')
+            except ValueError:
+                print('Invalid assignment')
+        elif len(expression) > 0:
+            try:
+                postfix_queue = infix_to_postfix_api.parse(expression)
+                print(calc_postfix_queue(postfix_queue))
+            except (ArithmeticError, ValueError, IndexError) as err:
+                print('Invalid expression')
+                print(err)
+            except (KeyError, NameError):
+                print('Unknown variable')
+        expression = input()
+
+
+def solve(expression: str, **variables):
+    if variables:
+        var_dict.update(variables)
+    return calc_postfix_queue(infix_to_postfix_api.parse(expression))
+
+
+def calc_postfix_queue(queue: deque):
+    stack = deque()
+    while queue:
+        elem = queue.popleft()
+        if elem not in infix_to_postfix_api.OPERATORS:
+            if is_var(elem):
+                if elem in var_dict:
+                    elem = var_dict[elem]
+                else:
+                    raise NameError
+            stack.append(float(elem))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(operations[elem](a, b))
+    result = stack.pop()
+    return int(result) if result // 1 == result else result  # return as int if it has no fraction
+
+
+def upd_dict_with_var(key: str, value):
+    if not is_var(key):
+        raise NameError
+    if is_var(value):  # in case there is var to var assignment
+        value = var_dict[value]
+    else:  # no need in float type without fractional part
+        value = int(value) if float(value) == int(value) else float(value)
+    var_dict[key] = value
+
+
+def is_var(var: str):  # check if variable name isn't empty and consists of latin letters only
+    return False if len(var) == 0 or not re.compile("^[a-zA-Z]+$").search(var) else True
+
+
+def is_command(expression_: str):
+    return True if expression_ and expression_.strip()[0] == '/' else False
+
+
+def exit_():
+    print('Bye!')
+    sys.exit()
+
+
+commands = {
+    '/exit': exit_,
+    '/help': lambda: print('''
+This program calculates math expressions
+
+Available operands:
+    - integer numbers
+    - floating numbers
+    - variables (variable must be declared before using)
+
+Available operators:
+    '+' - addition
+    '-' - subtraction
+    '*' - multiplication
+    '/' - division
+    '^' - exponentiation
+
+Available commands:
+    '/exit' - stops program running
+    '/help' - shows manual
+
+Variable declaration syntax:
+    variable_name = number
+    - variable_name must consist of english letters only
+    - variable_name is case sensitive
+    - number may be an integer or a float number
+
+Other syntax peculiarities:
+    - calculation is insensitive to tabs and spaces in expression
+    - multiple '+' and '-' signs between operations are supported
+''')
+}
+
+
+if __name__ == '__main__':
+    main()
